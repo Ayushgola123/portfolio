b41cd8a6424f83267898f43637c08adcdff17034
Ayush Gola <97296541+Ayushgola123@users.noreply.github.com>
2023-07-31 01:28:15 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
                 <div className={styles.container}>
                     <div className={styles.logo}>
                         <a href="#">
-                            <img src="/public/aylogo.jpg" alt="logo" />
+                            <img src="/public/aylogo.png" alt="logo" />
                         </a>
                     </div>
                     <div className={styles.options}>
