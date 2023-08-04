e7138ceddbb078c665dae371f4dbf878ef8d8c07
Ayush Gola <ayush.gola.ag@gmail.com>
2023-08-04 18:31:54 +0530

    third

--- components/Navbar.js
+++ components/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
                 <div className={styles.container}>
                     <div className={styles.logo}>
                         <a href="#">
-                            <img src="/assets/aylogo_adobe_express.svg" alt="logo" />
+                            <img src="aylogo.png" alt="logo" />
                         </a>
                     </div>
                     <div className={styles.options}>
